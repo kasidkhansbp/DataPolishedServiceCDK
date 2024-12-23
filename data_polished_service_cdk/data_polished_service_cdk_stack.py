7ad4e5bb1024913a4f97865e841713d2bb5c8b0e
kasid Khan <kasidkhan@gmail.com>
2024-12-23 10:33:12 +0100

    added alarm for Cloudwatch DLQ

--- data_polished_service_cdk/data_polished_service_cdk_stack.py
+++ data_polished_service_cdk/data_polished_service_cdk_stack.py
@@ -6,9 +6,11 @@ from aws_cdk import (
     aws_s3_notifications as s3_notification,
     aws_lambda as _lambda,
     aws_dynamodb as dynamodb,
-    aws_lambda_event_sources as lambda_event_sources
+    aws_lambda_event_sources as lambda_event_sources,
+    aws_cloudwatch as cloudwatch
 )
 from constructs import Construct
+
 
 class DataPolishedServiceCdkStack(Stack):
 
@@ -17,29 +19,33 @@ class DataPolishedServiceCdkStack(Stack):
 
         # The code that defines your stack goes here
 
-        my_bucket = s3.Bucket(self,"MyBucket")
+        my_bucket = s3.Bucket(self, "MyBucket")
 
         dlq = sqs.Queue(self, "MyDLQ")
 
         my_queue = sqs.Queue(self, "MyQueue", dead_letter_queue=sqs.DeadLetterQueue(max_receive_count=5))
 
-        #Enable notifications for the bucket
-        my_bucket.add_event_notification(s3.EventType.OBJECT_CREATED,s3_notification.SqsDestination(my_queue))
+        # Enable notifications for the bucket
+        my_bucket.add_event_notification(s3.EventType.OBJECT_CREATED, s3_notification.SqsDestination(my_queue))
 
         lambda_function = _lambda.Function(self, "myFunction",
-            runtime =_lambda.Runtime.PYTHON_3_8,
-            handler = "index.handler",
-            code=_lambda.Code.from_asset("lambda/"))
+                                           runtime=_lambda.Runtime.PYTHON_3_8,
+                                           handler="index.handler",
+                                           code=_lambda.Code.from_asset("lambda/"))
 
-        #Create a dynamoDB table
+        # Create a dynamoDB table
         dynamo_table = dynamodb.Table(self, "PolishedDataDBTable",
                                       partition_key=dynamodb.Attribute(name="Id",
-                                    type=dynamodb.AttributeType.STRING), read_capacity=5,write_capacity=5)
+                                                                       type=dynamodb.AttributeType.STRING),
+                                      read_capacity=5, write_capacity=5)
 
         # Grant the Lambda function permissions to write to DynamoDB
         dynamo_table.grant_write_data(lambda_function)
         # Grant the lambda function to read data from S3 file
         my_bucket.grant_read(lambda_function)
 
-        #Connect SQS to Lambda ( SQS to Lambda )
+        # Connect SQS to Lambda ( SQS to Lambda )
         lambda_function.add_event_source(lambda_event_sources.SqsEventSource(my_queue))
+
+        cloudwatch.Alarm(self, "DLQAlarm", metric=dlq.metric_approximate_number_of_messages_visible(), threshold=10,
+                         evaluation_periods=1, alarm_name="DLQMessageOverFlow")
